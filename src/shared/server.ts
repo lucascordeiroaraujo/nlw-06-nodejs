c4d8fa7812a34dac2112c4a10e3b5fb16263f514
Lucas Cordeiro Araujo <lucascordeiroaraujo@gmail.com>
2021-06-27 00:49:30 -0300

    :tada: Begin a project - project structure

---
+++ src/shared/server.ts
@@ -0,0 +1,17 @@
+import 'reflect-metadata'
+
+import express from 'express'
+
+import '@database/index'
+
+import cors from 'cors'
+
+const app = express()
+
+app.use(express.json())
+
+app.use(cors())
+
+app.listen(3000, () => {
+  console.log('Server is running')
+})
