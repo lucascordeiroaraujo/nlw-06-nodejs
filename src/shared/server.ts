5d935e03b40de263935d5497247b9d513b870dba
Lucas Cordeiro Araujo <lucascordeiroaraujo@gmail.com>
2021-06-27 01:08:20 -0300

    :goal_net: add class AppError

--- src/shared/server.ts
+++ src/shared/server.ts
@@ -1,10 +1,12 @@
 import 'reflect-metadata'
 
-import express from 'express'
+import express, { Request, Response, NextFunction } from 'express'
 
 import '@database/index'
 
 import cors from 'cors'
+
+import AppError from '@shared/errors/AppError'
 
 const app = express()
 
@@ -12,6 +14,22 @@ app.use(express.json())
 
 app.use(cors())
 
-app.listen(3000, () => {
-  console.log('Server is running')
+app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
+  if (err instanceof AppError) {
+    return response.status(err.statusCode).json({
+      status: 'error',
+      message: err.message,
+    })
+  }
+
+  console.error(err)
+
+  return response.status(500).json({
+    status: 'error',
+    message: 'Internal server error',
+  })
+})
+
+app.listen(3333, () => {
+  console.log('🚀 Server started on port 3333!')
 })
